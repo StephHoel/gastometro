d3092058d1541b199d8ba60f3e8262423632c81c
Steph Hoel <steph.hoel@gmail.com>
2025-06-01 00:06:17 -0300

    feat: enhance Calculator component with input handling, add clear and calculate functionality, and improve button styling

--- src/app/calculator.tsx
+++ src/app/calculator.tsx
@@ -1,11 +1,97 @@
-import { Text, View } from "react-native";
+import { CustomButton as Button } from "@/components/Button";
+import { CustomInput } from "@/components/CustomInput";
+import { Header } from "@/components/Header";
+import { text } from "@/constants/text";
+import { AlertService } from "@/services/AlertService";
+import { Divide, SetCurrency } from "@/utils/functions/MathFunctions";
+import { useRef, useState } from "react";
+import { Keyboard, Text, TextInput, View } from "react-native";
 
 export default function Calculator() {
+	const [answer, setAnswer] = useState<number | null>(null);
+
+	const [price, setPrice] = useState("");
+	const [quantity, setQuantity] = useState("");
+	const [unit, setUnit] = useState("");
+		
+	const inputRef1 = useRef<TextInput>(null);
+	const inputRef2 = useRef<TextInput>(null);
+	const inputRef3 = useRef<TextInput>(null);
+	
+	function clear() {
+		setPrice("")
+		setQuantity("")
+		setUnit("")
+	}
+
+	function handleToCalc() {
+		Keyboard.dismiss();
+
+		if (price === "" || quantity === "") {
+			return AlertService.ok("Erro", text.error.campos_nao_preenchidos)
+		}
+
+		setAnswer(Divide(price, quantity));
+	}
+
+	function handleToClear() {
+		Keyboard.dismiss();
+		clear()
+		setAnswer(null);
+	}
+
 	return (
 		<View className="flex-1 bg-slate-900">
-			<Text className="text-white text-center pt-2 pb-4">
-				Calculadora aqui em breve
-			</Text>
+			<Header />
+
+			<View className="mt-5 gap-5">
+				<CustomInput
+					nameField={"Preço"}
+					selfRef={inputRef1}
+					placeholder={"Preço da Embalagem"}
+					setItem={setPrice}
+					item={price}
+					onSubmit={() => inputRef2.current?.focus()}
+					returnKeyType={"next"}
+				/>
+
+				<CustomInput
+					nameField={"Quantidade"}
+					selfRef={inputRef2}
+					placeholder={"Quantidade na Embalagem"}
+					setItem={setQuantity}
+					item={quantity}
+					// onSubmit={() => inputRef3.current?.focus()}
+					// returnKeyType={"next"}
+					onSubmit={handleToCalc}
+					returnKeyType={"done"}
+				/>
+
+				{/* <CustomInput
+					nameField={"Unidade"}
+					selfRef={inputRef3}
+					placeholder={"Unidade de Medida (seletor)"}
+					setItem={setUnit}
+					item={unit}
+					onSubmit={handleToCalc}
+					returnKeyType={"done"}
+				/> */}
+
+				<Button onPress={handleToCalc}>
+					<Button.Text>Calcular</Button.Text>
+				</Button>
+
+				<Button type="Normal" onPress={handleToClear}>
+					<Button.Text>Limpar</Button.Text>
+				</Button>
+
+				<View className="border border-white rounded-2xl mx-5 p-4 gap-2 text-white">
+					<Text className="text-white">Preço por Unidade:</Text>
+					<Text className="text-white">
+						{answer == null ? "Resposta" : `${SetCurrency(answer)}`}
+					</Text>
+				</View>
+			</View>
 		</View>
 	);
 }
--- src/interfaces/CustomInputProps.ts
+++ src/interfaces/CustomInputProps.ts
@@ -1,7 +1,7 @@
 import type { TextInput } from "react-native";
 
 export interface CustomInputProps {
-  nameField: "Item" | "Quantidade" | "Preço"
+  nameField: "Item" | "Quantidade" | "Preço" | "Unidade"
   selfRef: React.RefObject<TextInput>;
   placeholder: string;
   setItem: (value: string) => void;
