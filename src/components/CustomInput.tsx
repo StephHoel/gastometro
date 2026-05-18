64d5113ca12007fc661bc2f6a0bb254cd01a8b68
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2026-05-17 21:34:04 -0300

    feat: update version to 1.4.1 and enhance CustomInput component for better handling of numeric input

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,59 +1,66 @@
-import type { CustomInputProps } from "@/interfaces/CustomInputProps";
-import { Text, TextInput, View } from "react-native";
-import colors from "tailwindcss/colors";
+import type { CustomInputProps } from "@/interfaces/CustomInputProps"
+import { Text, TextInput, View } from "react-native"
+import colors from "tailwindcss/colors"
 
 export function CustomInput({
-	nameField,
-	placeholder,
-	selfRef,
-	returnKeyType,
-	setItem,
-	item,
-	onSubmit,
-	keyboardType = "default",
+    nameField,
+    placeholder,
+    selfRef,
+    returnKeyType,
+    setItem,
+    item,
+    onSubmit,
+    keyboardType = "default",
 }: CustomInputProps) {
-	function handleChangeText(text: string) {
-		if (nameField === "Quantidade" || nameField === "Preço") {
-			let filtered = text.replace(/[^0-9.,]/g, "");
+    function handleChangeText(text: string) {
+        if (nameField === "Quantidade" || nameField === "Preço") {
+            let filtered = text.replace(/[^0-9.,]/g, "")
 
-			const sep = filtered.includes(",")
-				? ","
-				: filtered.includes(".")
-					? "."
-					: "";
-			
-			const [intPart, ...rest] = filtered.split(/[.,]/);
-			let decimalPart = rest.join("");
+            const sep = filtered.includes(",")
+                ? ","
+                : filtered.includes(".")
+                    ? "."
+                    : ""
 
-			if (sep) {
-				const maxDecimals = nameField === "Preço" ? 2 : 3;
-				decimalPart = decimalPart.slice(0, maxDecimals);
-				filtered = intPart + "." + decimalPart;
-			} else {
-				filtered = intPart;
-			}
-			setItem(filtered);
-		} else {
-			setItem(text);
-		}
-	}
+            const [intPart, ...rest] = filtered.split(/[.,]/)
+            let decimalPart = rest.join("")
 
-	return (
-		<View className="border border-white rounded-2xl mx-5 py-1 text-white flex-row">
-			<Text className="text-white p-2">{nameField}:</Text>
+            if (sep) {
+                const maxDecimals = nameField === "Preço" ? 2 : 3
+                decimalPart = decimalPart.slice(0, maxDecimals)
+                filtered = intPart + "." + decimalPart
+            } else {
+                filtered = intPart
+            }
+            setItem(filtered)
+        } else {
+            setItem(text)
+        }
+    }
 
-			<TextInput
-				className={"text-white flex-1 py-2 mr-3 outline-none"}
-				placeholderTextColor={colors.slate[400]}
-				placeholder={placeholder}
-				onChangeText={handleChangeText}
-				value={item}
-				ref={selfRef}
-				keyboardType={keyboardType}
-                onFocus={() => { if (item == "0.00" || item == "0") { setItem("") } }}
-				onSubmitEditing={onSubmit}
-				returnKeyType={returnKeyType}
-			/>
-		</View>
-	);
+    return (
+        <View className="border border-white rounded-2xl mx-5 py-1 text-white flex-row">
+            <Text className="text-white p-2">{nameField}:</Text>
+
+            <TextInput
+                className={"text-white flex-1 py-2 mr-3 outline-none"}
+                placeholderTextColor={colors.slate[400]}
+                placeholder={placeholder}
+                onChangeText={handleChangeText}
+                value={item}
+                ref={selfRef}
+                keyboardType={keyboardType}
+                onFocus={() => {
+                    if (
+                        (nameField === "Quantidade" || nameField === "Preço") &&
+                        (item == "0.00" || item == "0")
+                    ) {
+                        setItem("")
+                    }
+                }}
+                onSubmitEditing={onSubmit}
+                returnKeyType={returnKeyType}
+            />
+        </View>
+    )
 }
