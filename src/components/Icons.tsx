100e92aa31e31d30ded21f882f1479a4b8fb5036
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2026-06-06 21:09:36 -0300

    chore: update dependencies and improve icon handling
    - Updated dependencies in package.json for expo, nativewind, and react-native-svg.
    - Refactored Icons component to use SVG assets instead of Ionicons.
    - Added TypeScript declaration for SVG imports.
    - Created tests for Form and Header components to improve coverage.
    - Mocked components in tests to isolate functionality.
    - Adjusted tsconfig to include .d.ts files for better type handling.

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,49 +1,55 @@
-import type { IconsProps } from "@/interfaces/IconsProps"
-import IonIcons from "@expo/vector-icons/Ionicons"
 import React from 'react'
+import type { IconsProps } from "@/interfaces/IconsProps"
+
+import Add from '@/assets/svgs/add-circle-outline.svg'
+import Back from '@/assets/svgs/arrow-back.svg'
+import Calculator from '@/assets/svgs/calculator.svg'
+import CheckboxChecked from '@/assets/svgs/checkbox-outline.svg'
+import CheckboxUnchecked from '@/assets/svgs/square-outline.svg'
+import Edit from '@/assets/svgs/pencil.svg'
+import Home from '@/assets/svgs/home.svg'
+import Share from '@/assets/svgs/share-social-outline.svg'
+import Trash from '@/assets/svgs/trash-outline.svg'
+import Whatsapp from '@/assets/svgs/logo-whatsapp.svg'
 
 export function AddIcon({ size = 24, color = "white" }: IconsProps) {
-  return <IonIcons name="add-circle-outline" size={size} color={color} />
+  return <Add width={size} height={size} color={color} />
 }
 
 export function BackIcon({ size = 24, color = "white" }: IconsProps) {
-  return <IonIcons name="arrow-back" size={size} color={color} />
+  return <Back width={size} height={size} color={color} />
 }
 
 export function CalculatorIcon({ size = 24, color = "white" }: IconsProps) {
-  return <IonIcons name="calculator" size={size} color={color} />
+  return <Calculator width={size} height={size} color={color} />
 }
 
 export function BroomIcon({ size = 24, color = "white" }: IconsProps) {
-  return <IonIcons name="trash-outline" size={size} color={color} />
+  return <Trash width={size} height={size} color={color} />
 }
 
-export function CheckboxIcon({ checked, size = 24 }: IconsProps) {
-  return (
-    <IonIcons
-      name={checked ? "checkbox" : "square"}
-      color={checked ? "#a3e635" : "white"}
-      size={size}
-    />
-  )
+export function CheckboxIcon({ checked, size = 30 }: IconsProps) {
+  return checked
+    ? <CheckboxChecked width={size} height={size} color="#A3E635" />
+    : <CheckboxUnchecked width={size} height={size} color="white" />
 }
 
 export function DeleteIcon({ size = 24, color = "white" }: IconsProps) {
-  return <IonIcons name="trash-outline" size={size} color={color} />
+  return <Trash width={size} height={size} color={color} />
 }
 
 export function EditIcon({ size = 24, color = "white" }: IconsProps) {
-  return <IonIcons name="pencil" size={size} color={color} />
+  return <Edit width={size} height={size} color={color} />
 }
 
 export function HomeIcon({ size = 24, color = "white" }: IconsProps) {
-  return <IonIcons name="home" size={size} color={color} />
+  return <Home width={size} height={size} color={color} />
 }
 
 export function ShareIcon({ size = 24, color = "white" }: IconsProps) {
-  return <IonIcons name="share-social-outline" size={size} color={color} />
+  return <Share width={size} height={size} color={color} />
 }
 
 export function WhatsappIcon() {
-  return <IonIcons name="logo-whatsapp" size={18} color={"black"} />
+  return <Whatsapp width={18} height={18} color={"black"} />
 }
