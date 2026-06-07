230c7fdfdd9c64f2c886534b6193393ff7a1760e
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2026-06-07 13:01:26 -0300

    feat: replace hardcoded black color with color constant in multiple components

--- src/components/TouchableIcons.tsx
+++ src/components/TouchableIcons.tsx
@@ -2,6 +2,7 @@ import { AddIcon, BackIcon, TrashIcon, ShareIcon } from "@/components/Icons"
 import type { IconProps } from "@/interfaces/IconProps"
 import { TouchableOpacity, View } from "react-native"
 import React from 'react'
+import { colors } from '@/constants/color'
 
 function IconButton({ action, children, containerClass, wrapperClass }: {
   action?: () => void
@@ -31,7 +32,7 @@ export function Add({ action }: IconProps) {
       wrapperClass="absolute bottom-6 right-6 z-50"
       containerClass="bg-lime-400 rounded-full w-14 h-14 items-center justify-center"
     >
-      <AddIcon size={35} color="black" />
+      <AddIcon size={35} color={colors.black} />
     </IconButton>
   )
 }
