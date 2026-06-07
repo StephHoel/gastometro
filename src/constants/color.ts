a800cb754221bad8f389494cf6bf8d78910c0e33
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2026-06-07 12:58:55 -0300

    feat: standardize color constants formatting in color.ts

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -1,7 +1,7 @@
 export const colors = {
-  "background": "#0F172A",
-  "inactive": "#94A3B8",
-  "active": "#A3E635",
-  "white": "WHITE",
-  "black": "BLACK",
+  background: '#0F172A',
+  inactive: '#94A3B8',
+  active: '#A3E635',
+  white: '#FFFFFF',
+  black: '#000000',
 } 
