508d82236ba5aad04bedaad8567b5eb03a4c5e9c
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2026-06-13 18:04:40 -0300

    feat: update app configuration and remove app.json for improved structure and versioning

--- app.config.js
+++ app.config.js
@@ -1,9 +1,68 @@
 export default {
-  expo: {
-    name: "Gastômetro",
-    slug: "gastometro",
-  },
-  android: {
-    package: "com.stephhoel.gastometro"
-  },
+  "expo": {
+    "name": "Gastômetro",
+    "slug": "gastometro",
+    "version": "1.5.0",
+    "orientation": "portrait",
+    "icon": "./assets/images/icon.png",
+    "scheme": "gastometro",
+    "userInterfaceStyle": "dark",
+    "android": {
+      "package": "com.stephhoel.gastometro",
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/images/adaptive-icon-foreground.png",
+        "backgroundColor": "#0F172A"
+      },
+      "predictiveBackGestureEnabled": false
+    },
+    "web": {
+      "bundler": "metro",
+      "output": "static",
+      "favicon": "./assets/images/favicon.png",
+      "build": {
+        "babel": {
+          "include": [
+            "react-native-web"
+          ]
+        }
+      }
+    },
+    "plugins": [
+      "expo-router",
+      [
+        "expo-build-properties",
+        {
+          "android": {
+            "minSdkVersion": 29,
+            "enableProguardInReleaseBuilds": true,
+            "enableShrinkResourcesInReleaseBuilds": true,
+            "enableSeparateBuildPerCPUArchitecture": true,
+            "extraProguardRules": "-assumenosideeffects class android.util.Log { *; }"
+          }
+        }
+      ],
+      [
+        "expo-splash-screen",
+        {
+          "backgroundColor": "#0F172A",
+          "android": {
+            "image": "./assets/images/splash-icon.png"
+          }
+        }
+      ]
+    ],
+    "experiments": {
+      "typedRoutes": true,
+      "baseUrl": "/gastometro"
+    },
+    "extra": {
+      "router": {
+        "origin": false
+      },
+      "eas": {
+        "projectId": "7df77fa7-4459-4505-baba-b8c6c2d0090c"
+      }
+    },
+    "owner": "stephhoel"
+  }
 }
