b3f618251a27e5214a9f3b870a17cc2c8e07d64f
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2026-06-21 15:00:45 -0300

    feat: bump version to 1.7.0 and update changelog with mini-spec 10 implementation details

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Gastômetro",
     slug: "gastometro",
-    version: "1.6.0",
+    version: "1.7.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "gastometro",
